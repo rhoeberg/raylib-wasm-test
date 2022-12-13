2047e818630fc3c9d0bce6c3ece7e4a963cef408
rhoeberg <contact@rhoeberg.com>
2022-12-13 19:04:29 +0100

    testing web

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-rem emcc -o game.html main.cpp -Os -Wall ./lib/libraylib.a -I. -I./include -L. -L./lib -s USE_GLFW=3 -DPLATFORM_WEB
+emcc -o game.html main.cpp -Os -Wall ./3rdparty/lib/libraylib.a -I. -I./3rdparty/include -L. -L./3rdparty/lib -s USE_GLFW=3 -DPLATFORM_WEB
 
 REM asyncify
-emcc -o index.html main.cpp -Os -Wall ./3rdparty/lib/libraylib.a -I. -I./3rdparty/include -L. -L./3rdparty/lib -s USE_GLFW=3 -s ASYNCIFY -s EXPORTED_RUNTIME_METHODS=ccall --shell-file minshell.html -DPLATFORM_WEB
+rem emcc -o index.html main.cpp -Os -Wall ./3rdparty/lib/libraylib.a -I. -I./3rdparty/include -L. -L./3rdparty/lib -s USE_GLFW=3 -s MODULARIZE=1 -s EXPORT_NAME="'MyModule'" -s EXPORTED_RUNTIME_METHODS=ccall --shell-file minshell.html -DPLATFORM_WEB
 
 rem emcc -o game.html game.c -Os -Wall ./path-to/libraylib.a -I. -Ipath-to-raylib-h -L. -Lpath-to-libraylib-a -s USE_GLFW=3 -s ASYNCIFY --shell-file path-to/shell.html -DPLATFORM_WEB
--- main.cpp
+++ main.cpp
@@ -23,45 +23,43 @@
 
 #include "raylib.h"
 
-//------------------------------------------------------------------------------------
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+void UpdateDrawFrame();
+
 // Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
     // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+    SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+        UpdateDrawFrame();
     }
+#endif
 
     // De-Initialization
-    //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
 
     return 0;
+}
+
+void UpdateDrawFrame()
+{
+	// BeginDrawing();
+	// ClearBackground(RAYWHITE);
+	// DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+	// EndDrawing();
 }
